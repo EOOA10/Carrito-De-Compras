2dfc582e1a38f9fe424e9470711de043d51faee6
Edison Ordoñez <edisonomarordonez@gmail.com>
2023-02-18 23:02:02 -0600

    Conexion a la BD y mostrar Usuarios

---
+++ CarritoCompras/CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Referencia a la CapaEntidad
+using CapaEntidad;
+
+//Referencia para lograr la conexion a SQL
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        public List<Usuario> Listar() 
+        {
+            List<Usuario> lista= new List<Usuario>();
+
+            try
+            {
+                //Conexion a la BDD haciendo referencia a la clase Conexion
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "SELECT IdUsuario,Nombre,Apellido,Correo,Clave,Reestablecer,Activo FROM USUARIO";
+                    
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Usuario() 
+                                {
+                                    IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                    Nombre = dr["Nombre"].ToString(),
+                                    Apellido = dr["Apellido"].ToString(),
+                                    Correo = dr["Correo"].ToString(),
+                                    Clave = dr["Clave"].ToString(),
+                                    Reestablecer = Convert.ToBoolean(dr["Reestablecer"]),
+                                    Activo = Convert.ToBoolean(dr["Activo"])
+                                }
+                                );
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Usuario>();
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ CarritoCompras/CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Referencias a las capas Datos y Entidad para hacer llamados de los elementos que estan dentro de las capas
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        //Acceso a todos los metodos de la clase CD_Usuarios
+        private CD_Usuarios objCapaDato = new CD_Usuarios();
+
+        //Metodo que retorna el listado de la clase CD_Usuarios
+        public List<Usuario> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+    }
+}
